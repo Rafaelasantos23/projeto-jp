52e4942a0d508dffb67dc5b22d1d9d30ba8e6dcf
Rafaela <143849661+Rafaelasantos23@users.noreply.github.com>
2024-03-05 21:22:40 -0300

    informar qual foi a escolha da maquina

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const result = document.querySelector('.result')
 
 const scoringHuman = document.querySelector('.minhap')
@@ -9,47 +8,65 @@ let machineNamber = 0
 
 let image = document.querySelector('.img')
 
+const choiceMachine = document.querySelector('.choiceMachine')
+const choiceHuman1 = document.querySelector('.choicehuman1')
 
-const PlayHuman = ( choiceHuman => {
 
-    PlayTheGame (choiceHuman,Playmachine())
+const PlayHuman = (choiceHuman => {
+
+    PlayTheGame(choiceHuman, Playmachine())
+
 })
 
-const Playmachine = ()  =>{
-const choices = ['rock', 'paper', 'scissors']
-const radomNamber = Math.floor (Math.random() * 3)
+const Playmachine = () => {
+    const choices = ['rock', 'paper', 'scissors']
+    const radomNamber = choices[Math.floor(Math.random() * choices.length)]
 
-return choices [radomNamber]
-}
-
-
-const PlayTheGame = (human,machine) => {
-console.log(' humano: ' +  human  +  ' maquina: ' +  machine)
-
-if(human === machine){
-result.innerHTML = 'Empatou!'
-
-image.src = "./assests/sup.png"
-
-}
-else if(human === 'paper' && machine === 'rock' ||
-        human === 'rock' && machine === 'scissors' || 
-        human === 'scissors' && machine === 'paper'){
-            result.innerHTML = ' Você ganhou!'
-
-            humanNamber++
-            scoringHuman.innerHTML = humanNamber
-
-            image.src="./assests/fel.png"
-
-
-
-        }
-         else {result.innerHTML = ' A Maquina ganhou!'
-        
-                machineNamber++
-                scoringMachine.innerHTML = machineNamber
-
-                image.src ="./assests/assus.png"
-        }
+    if (radomNamber == 'rock') {
+        choiceMachine.innerHTML = "A maquina escolheu : rock";
     }
+
+    if (radomNamber == 'paper') {
+        choiceMachine.innerHTML = "A maquina escolheu  : paper";
+    }
+
+    if (radomNamber == 'scissors') {
+        choiceMachine.innerHTML = "A maquina escolheu  : scissors";
+    }
+
+
+    return radomNamber
+}
+
+
+const PlayTheGame = (human, machine) => {
+    console.log(' humano: ' + human + ' maquina: ' + machine)
+
+    if (human === machine) {
+        result.innerHTML = 'Empatou!'
+
+        image.src = "./assests/sup.png"
+
+    }
+    else if (human === 'paper' && machine === 'rock' ||
+        human === 'rock' && machine === 'scissors' ||
+        human === 'scissors' && machine === 'paper') {
+        result.innerHTML = ' Você ganhou!'
+
+        humanNamber++
+        scoringHuman.innerHTML = humanNamber
+
+        image.src = "./assests/fel.png"
+
+
+
+    }
+    else {
+        result.innerHTML = ' A Maquina ganhou!'
+
+        machineNamber++
+        scoringMachine.innerHTML = machineNamber
+
+        image.src = "./assests/assus.png"
+    }
+}
